d839369eeddac8196411ad85499a7585355c25a4
benellis3 <benellis@robots.ox.ac.uk>
2022-03-02 10:50:00 +0000

    Add random agent obs and state saving

--- smac/examples/random_agents.py
+++ smac/examples/random_agents.py
@@ -4,6 +4,9 @@ from __future__ import print_function
 
 from smac.env import StarCraft2Env
 import numpy as np
+from pathlib import Path
+
+DIR = Path(__file__).parent.parent.parent / "supervised_obs_and_states"
 
 
 def main():
@@ -13,13 +16,14 @@ def main():
     n_actions = env_info["n_actions"]
     n_agents = env_info["n_agents"]
 
-    n_episodes = 10
+    n_episodes = 2
 
     for e in range(n_episodes):
         env.reset()
         terminated = False
         episode_reward = 0
-
+        obs_list = []
+        state_list = []
         while not terminated:
             obs = env.get_obs()
             state = env.get_state()
@@ -34,7 +38,13 @@ def main():
 
             reward, terminated, _ = env.step(actions)
             episode_reward += reward
+            obs_list.append(obs)
+            state_list.append(state)
 
+        stacked_obs_list = np.stack(obs_list)
+        stacked_state_list = np.stack(state_list)
+        np.save(DIR / f"episode_{e}_obs.npy", stacked_obs_list)
+        np.save(DIR / f"episode_{e}_state.npy", stacked_state_list)
         print("Total reward in episode {} = {}".format(e, episode_reward))
 
     env.close()
