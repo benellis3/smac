6f10e5c9967e38291a23ccfd86760d31ff89a37b
benellis3 <benellis@robots.ox.ac.uk>
2023-09-21 13:55:20 +0100

    Add stop action

--- smac/env/starcraft2/minismac_sc2_wrapper.py
+++ smac/env/starcraft2/minismac_sc2_wrapper.py
@@ -32,6 +32,7 @@ class MiniSMACStarCraft2Env(StarCraft2Env):
             1: 4,
             2: 3,
             3: 5,
+            4: 1,
         }
 
     def step(self, actions):
@@ -40,7 +41,8 @@ class MiniSMACStarCraft2Env(StarCraft2Env):
         for i, a in enumerate(actions):
             unit = self.get_unit_by_id(i)
             if unit.health > 0:
-                actions[i] = self.actions_map[a] if a in self.actions_map else a
+                # SMAC has an extra action.
+                actions[i] = self.actions_map[a] if a in self.actions_map else a - 1
                 avail_actions = np.array(self.get_avail_agent_actions(i))
                 if avail_actions[actions[i]] != 1:
                     actions[i] = np.random.choice(self.n_actions, 1, p=avail_actions / avail_actions.sum())
